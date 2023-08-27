388271ecc3d835eacfe40cf19f7f68443a13785f
Vitoria Albuquerque <albuquerquevitoria14@gmail.com>
2023-08-26 22:45:16 -0300

    LS-7: Implementação cadastro do usuário

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('valida_cadastro/', views.valida_cadastro, name= 'valida_cadastro'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# deflogin(request):
-    #return HttpResponse('login')
+from .models import Usuario
+from django.shortcuts import redirect
+from hashlib import sha256
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    status = request.GET.get('status')
+    return render(request, 'cadastro.html', {'status': status})
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    senha_repeticao = request.POST.get('senha_repeticao')
+
+    usuario = Usuario.objects.filter(email = email)
+
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=1')
+
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=2')
+    
+    if senha != senha_repeticao:
+        return redirect('/auth/cadastro/?status=5')
+    
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome, senha=senha, email=email)
+        usuario.save()
+
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
