1b44936f7ab63e165f85db75aa5eea13e5610414
Matheus <Perreiramatheus98@gmail.com>
2023-09-14 14:46:45 -0300

    Pagina home

--- projeto_livraria/urls.py
+++ projeto_livraria/urls.py
@@ -1,9 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('livro/', include('livro.urls')),
     path('auth/', include('usuarios.urls'))
 
 ]
